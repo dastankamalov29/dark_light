8734057053bbc64522cc32fcce29d1ce9ee8522f
kamalovdastan <102924881+dastankamalov29@users.noreply.github.com>
2024-04-23 15:02:32 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+
+import 'package:dark_light_mode/pages/home_page.dart';
+import 'package:dark_light_mode/theme/theme.dart';
+import 'package:dark_light_mode/theme/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
+
+    );
+  }
+}
---
+++ lib/theme/theme_provider.dart
@@ -0,0 +1,27 @@
+
+
+
+
+import 'package:dark_light_mode/theme/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ThemeProvider with ChangeNotifier{
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData => _themeData;
+
+
+  set themeData(ThemeData themeData){
+    _themeData = themeData;
+    notifyListeners();
+  }
+  void toggleTheme(){
+    if (_themeData == lightMode) {
+      themeData = darkMode;
+      
+    } else {
+      themeData = lightMode;
+    }
+  }
+}
